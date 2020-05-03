27c25ff0436d1579e5768bed54f23ae90ef3d697
Danilo <danilomaioli@gmail.com>
2020-05-03 20:39:50 -0300

    Commit

--- server/tf2/tf2-bot-server/index.js
+++ server/tf2/tf2-bot-server/index.js
@@ -35,6 +35,8 @@ class TB {
 
   static Init()
   {
+    return
+    
     this.ReadLocalStorage();
 
     TBBackpack.GetItemInfo("https://backpack.tf/stats/Unique/Mann%20Co.%20Supply%20Crate%20Key/Tradable/Craftable").then((info) => {
